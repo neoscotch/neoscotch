93e03f9e0f1b538a6a9b07b798d964a2e367701f
Adam Mashburn <adam.c.mashburn@gmail.com>
2021-11-30 19:38:59 -0600

    fixed root and added file icons

--- my-env-files/root-dot-profile.ps1
+++ my-env-files/root-dot-profile.ps1
@@ -1,42 +1,30 @@
-Set-PoshPrompt -Theme blue-owl
-
-$Host.UI.RawUI.WindowTitle = "Windows Powershell " + $Host.Version;
-
-Get-Content ascii.txt
-
+# For Root
+Set-PoshPrompt -Theme lambda
+Import-Module Terminal-Icons
 Clear-Host
 
-Write-Host -ForegroundColor Green      ".     .       .  .   . .   .   . .    +  ."
-Write-Host -ForegroundColor Green      "  .     .  :     .    .. :. .___---------___."
-Write-Host -ForegroundColor Green      "       .  .   .    .  :.:. _ .^ .^ ^.  '.. : -_. ."
-Write-Host -ForegroundColor Green      "    .  :       .  .  .:../:            . .^  :.:\."
-Write-Host -ForegroundColor Green      "        .   . :: +. :.:/: .   .    .        . . .:\"
-Write-Host -ForegroundColor Green      " .  :    .     . _ :::/:               .  ^ .  . .:\"
-Write-Host -ForegroundColor Green      "  .. . .   . - : :.:./.                        .  .:\"
-Write-Host -ForegroundColor Green      "  .      .     . :..|:                    .  .  ^. .:|"
-Write-Host -ForegroundColor Green      "    .       . : : ..||        .                . . !:|"
-Write-Host -ForegroundColor Green      "  .     . . . ::. ::\(                           . :)/"
-Write-Host -ForegroundColor Green      " .   .     : . : .:.|. ######              .#######::|"
-Write-Host -ForegroundColor Green      "  :.. .  :-  : .:  ::|.#######           ..########:|"
-Write-Host -ForegroundColor Green      " .  .  .  ..  .  .. :\ ########          :######## :/"
-Write-Host -ForegroundColor Green      "  .        .+ :: : -.:\ ########       . ########.:/"
-Write-Host -ForegroundColor Green      "    .  .+   . . . . :.:\. #######       #######..:/"
-Write-Host -ForegroundColor Green      "      :: . . . . ::.:..:.\           .   .   ..:/"
-Write-Host -ForegroundColor Green      "   .   .   .  .. :  -::::.\.       | |     . .:/"
-Write-Host -ForegroundColor Green      "      .  :  .  .  .-:. :.::.\             ..:/"
-Write-Host -ForegroundColor Green      " .      -.   . . . .: .:::.:.\.           .:/"
-Write-Host -ForegroundColor Green      ".   .   .  :      : ....::_:..:\   ___.  :/"
-Write-Host -ForegroundColor Green      "   .   .  .   .:. .. .  .: :.:.:\       :/"
-Write-Host -ForegroundColor Green      "     +   .   .   : . ::. :.:. .:.|\  .:/|"
-Write-Host -ForegroundColor Green      "     .         +   .  .  ...:: ..|  --.:|"
-Write-Host -ForegroundColor Green      ".      . . .   .  .  . ... :..:.. (  ..)"
-Write-Host -ForegroundColor Green      " .   .       .      :  .   .: ::/  .  .::\"
-
-$HOSTS = "$env:SystemRoot\system32\drivers\etc\hosts";
-$Desktop = "$env:USERPROFILE\Desktop"
-$Documents = "$env:USERPROFILE\Documents"
-$TimestampServer = "http://timestamp.digicert.com"
-
-Set-Location C:\dev;
-
-Import-Module Terminal-Icons
+Write-Host -ForegroundColor DarkRed ".     .       .  .   . .   .   . .    +  ."
+Write-Host -ForegroundColor DarkRed "  .     .  :     .    .. :. .___---------___."
+Write-Host -ForegroundColor DarkRed "       .  .   .    .  :.:. _ .^ .^ ^.  '.. : -_. ."
+Write-Host -ForegroundColor DarkRed "    .  :       .  .  .:../:            . .^  :.:\."
+Write-Host -ForegroundColor DarkRed "        .   . :: +. :.:/: .   .    .        . . .:\"
+Write-Host -ForegroundColor DarkRed " .  :    .     . _ :::/:               .  ^ .  . .:\"
+Write-Host -ForegroundColor DarkRed "  .. . .   . - : :.:./.                        .  .:\"
+Write-Host -ForegroundColor DarkRed "  .      .     . :..|:                    .  .  ^. .:|"
+Write-Host -ForegroundColor DarkRed "    .       . : : ..||        .                . . !:|"
+Write-Host -ForegroundColor DarkRed "  .     . . . ::. ::\(                           . :)/"
+Write-Host -ForegroundColor DarkRed " .   .     : . : .:.|. ######              .#######::|"
+Write-Host -ForegroundColor DarkRed "  :.. .  :-  : .:  ::|.#######           ..########:|"
+Write-Host -ForegroundColor DarkRed " .  .  .  ..  .  .. :\ ########          :######## :/"
+Write-Host -ForegroundColor DarkRed "  .        .+ :: : -.:\ ########       . ########.:/"
+Write-Host -ForegroundColor DarkRed "    .  .+   . . . . :.:\. #######       #######..:/"
+Write-Host -ForegroundColor DarkRed "      :: . . . . ::.:..:.\           .   .   ..:/"
+Write-Host -ForegroundColor DarkRed "   .   .   .  .. :  -::::.\.       | |     . .:/"
+Write-Host -ForegroundColor DarkRed "      .  :  .  .  .-:. :.::.\             ..:/"
+Write-Host -ForegroundColor DarkRed " .      -.   . . . .: .:::.:.\.           .:/"
+Write-Host -ForegroundColor DarkRed ".   .   .  :      : ....::_:..:\   ___.  :/"
+Write-Host -ForegroundColor DarkRed "   .   .  .   .:. .. .  .: :.:.:\       :/"
+Write-Host -ForegroundColor DarkRed "     +   .   .   : . ::. :.:. .:.|\  .:/|"
+Write-Host -ForegroundColor DarkRed "     .         +   .  .  ...:: ..|  --.:|"
+Write-Host -ForegroundColor DarkRed ".      . . .   .  .  . ... :..:.. (  ..)"
+Write-Host -ForegroundColor DarkRed " .   .       .      :  .   .: ::/  .  .::\"
